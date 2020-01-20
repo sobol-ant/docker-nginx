0028b7fdf69d0147075ed21681083d0346a86d6b
unknown <anton.sobal@gmail.com>
2020-01-20 17:34:59 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <p className="App-intro">learn react</p>
+          <p className="App-intro">Works!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
