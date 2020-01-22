72bb56bf77983c7072a1e09b401a966adf8451c5
Anton Sobol <anton.sobal@gmail.com>
2020-01-22 12:49:32 +0300

    Some text updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <p className="App-intro">Deployed using github + travis + heroku!</p>
+          <p className="App-intro">Coming soon...</p>
       </header>
     </div>
   );
