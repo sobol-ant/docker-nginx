4ccff526207d0d52bc438262cef556accddb7b89
Anton Sobol <anton.sobal@gmail.com>
2020-01-22 11:17:21 +0300

    Text and tests update

--- src/App.test.js
+++ src/App.test.js
@@ -8,3 +8,9 @@ test('renders learn react link', () => {
     expect(linkElement).toBeInTheDocument();
 });
 
+test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/using github/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
